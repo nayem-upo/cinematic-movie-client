af6cc64a1e3fdf8e500c739499c6dc7fc4314bde
nayem-upo <nayemislam0181@gmail.com>
2023-10-09 12:38:14 +0600

    middleware configured

--- middleware.ts
+++ middleware.ts
@@ -6,22 +6,8 @@ export default authMiddleware({
 
 export const config = {
     matcher: [
-        '/dashboard',
+        // '/dashboard',
         '/(api|trpc)(.*)',
-        // '/details/1',
-        // '/details/2',
-        // '/details/3',
-        // '/details/4',
-        // '/details/5',
-        // '/details/6',
-        // '/details/7',
-        // '/details/8',
-        // '/details/9',
-        // '/details/10',
-        // '/details/11',
-        // '/details/12',
-        // '/details/13',
-        // '/details/14',
     ],
     public: [
         '/',
